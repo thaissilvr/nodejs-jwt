df4c891449e4c3304edf6cab25e7497d4b2dc30f
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-05-03 08:17:43 -0300

    Adicionando estratégia de autenticação para sistema de login

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const { estrategiasAutenticacao } = require('./src/usuarios');
+
+
 app.use(
   bodyParser.urlencoded({
     extended: true
