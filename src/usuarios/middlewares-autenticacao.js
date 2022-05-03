2a9fab1cebfc4e830ef890e348ddcf8f2290d13e
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-05-03 10:40:07 -0300

    Tratando erros de login.

---
+++ src/usuarios/middlewares-autenticacao.js
@@ -0,0 +1,28 @@
+const { restart } = require("nodemon");
+const passport = require("passport");
+
+module.exports = {
+    local:
+        (req, res, next) => {
+            passport.authenticate(
+                "local",
+                { session: false },
+                (error, usuario, info) => {
+                    if (error && error.name === 'InvaliadArgumentError') {
+                        return restart.status(401).json({ error: error.message })
+                    }
+
+                    if (error) {
+                        return res.status(500).send({ error: error.message })
+                    }
+
+                    if (!usuario) {
+                        return res.status(401).json();
+                    }
+
+                    req.user = usuario;
+                    return next()
+                }
+            )(req, res, next);
+        }
+}
