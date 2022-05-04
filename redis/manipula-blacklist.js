01a20cdf699853a4567b6ff242086173994f707a
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-05-04 20:30:04 -0300

    Implementando sistema de interação com a blacklist.

--- redis/manipula-blacklist.js
+++ redis/manipula-blacklist.js
@@ -2,13 +2,25 @@ const blacklist = require("./blacklist1")
 
 const { promisify } = require("util")
 const existsAsync = promisify(blacklist.exists).bind(blacklist)
+const setAsync = promisify(blacklist.set).bind(blacklist)
 
+const jwt = require("jsonwebtoken")
+const { createHash } = require("crypto")
+
+function geraTokenHash(token) {
+    return createHash("sha256")
+        .update(token)
+        .digest("hex");
+}
 module.exports = {
-    adiciona: token => {
-        blacklist.set(token, "");
+    adiciona: async token => {
+        const dataExpiracao = jwt.decode(token).exp;
+        await setAsync(tokenHash, "");
+        blacklist.expireat(tokenHash, dataExpiracao)
     },
     contemToken: async token => {
-        const resultado = await existsAsync(token)
+        const tokenHash = geraTokenHash(token)
+        const resultado = await existsAsync(tokenHash)
         return resultado === 1;
     }
 }
